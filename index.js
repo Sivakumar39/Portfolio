ac527c911d560a24a6fc6fe58cb639d9286d60c7
Arjunsiva1999 <66900410+Arjunsiva1999@users.noreply.github.com>
2021-01-22 14:38:21 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.animate',{
+strings:[
+  "Hello, I'm Sivakumar",
+  "I'm a Graphic Designer",
+  "Certified from Udemy ",
+  "I also Know Web Development"
+],
+typeSpeed:50,
+backspeed:50,
+loop:true
+});
